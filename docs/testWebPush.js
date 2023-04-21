83f063b57628e3977d908b436a4f4c96d40d3686
Deon1029 <seonbihanbin@gmail.com>
2023-04-21 15:48:18 +0900

    import test

---
+++ docs/testWebPush.js
@@ -0,0 +1,14 @@
+import webpush from "web-push";
+
+// const keys = webpush.generateVAPIDKeys();
+// console.log(keys)
+// if("serviceWoker" in navigatior){
+// 	navigator.serviceWorker.register("my-service-worker.js");
+// }
+// self.addEventListener("push", (event) => {
+// 	const title = event.data.text();
+//   	event.waitUntil(self.registration.showNotification(title));
+// }
+
+
+// console.log('asdf')
